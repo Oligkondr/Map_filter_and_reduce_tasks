1e4ac41b4f905d8a0f86699b3ea2b551d0d826a8
Oligkondr <oligkondr@mail.ru>
2024-04-01 15:29:36 +0300

    New 9 and 10

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 // Task 1
-console.log('-----> 1 <-----');
-
 const task1 = (n) => n.reduce((acc, num, i) => {
     if (num % 3 === 0) {
         acc.push({
@@ -11,30 +9,21 @@ const task1 = (n) => n.reduce((acc, num, i) => {
     return acc;
 }, []);
 
-console.log(task1([1, 2, 3, 4, 5, 6, 7, 8, 9]));
-console.log('');
+// console.log(task1([1, 2, 3, 4, 5, 6, 7, 8, 9]));
 
 // Task 2
-console.log('-----> 2 <-----');
-
 const task2 = (n) => n.filter((num) => num % 2 === 0);
 
-console.log(task2([1, 2, 3, 4, 5, 6, 7, 8, 9]));
-console.log('');
+// console.log(task2([1, 2, 3, 4, 5, 6, 7, 8, 9]));
 
 // Task 3
-console.log('-----> 3 <-----');
-
 const task3 = (words) => words
   .map((word) => (word[0]).toUpperCase() + word.slice(1))
   .join(' ');
 
-console.log(task3(['В', 'лесу', 'родилась', 'елочка']));
-console.log('');
+// console.log(task3(['В', 'лесу', 'родилась', 'елочка']));
 
 // Task 4
-console.log('-----> 4 <-----');
-
 const task4 = (str) => str
   .split(' ')
   .map((word) => word.length > 1
@@ -42,12 +31,9 @@ const task4 = (str) => str
     : word)
   .join(' ');
 
-console.log(task4('В лесу родилась елочка'));
-console.log('');
+// console.log(task4('В лесу родилась елочка'));
 
 // Task 5
-console.log('-----> 5 <-----');
-
 const task5 = (nums) => nums.reduce((acc, num) => {
     if (num > 0) {
         acc.positive += 1;
@@ -57,20 +43,15 @@ const task5 = (nums) => nums.reduce((acc, num) => {
     return acc;
 }, {'positive': 0, 'negative': 1});
 
-console.log(task5([-5, -4, -3, -2, -1, 1, 2, 3, 4, 5, 6, 7, 8, 9]));
-console.log('');
+// console.log(task5([-5, -4, -3, -2, -1, 1, 2, 3, 4, 5, 6, 7, 8, 9]));
 
 // Task 6
-console.log('-----> 6 <-----');
 
 const task6 = (nums) => (nums.filter((num, i) => num > nums[i + 1])).length;
 
-console.log(task6([24, 35, 29, 44, 8, 22, 4]));
-console.log('');
+// console.log(task6([24, 35, 29, 44, 8, 22, 4]));
 
 // Task 7
-console.log('-----> 7 <-----');
-
 const task7 = (team1, team2) => {
     return !!(team1
       .filter((player) => team2.includes(player))).length;
@@ -80,13 +61,10 @@ const team1 = [169, 174, 182, 173, 177];
 const team2 = [172, 150, 167, 174, 170];
 const team3 = [172, 150, 167, 172, 170];
 
-console.log(task7(team1, team2));
-console.log(task7(team1, team3));
-console.log('');
+// console.log(task7(team1, team2));
+// console.log(task7(team1, team3));
 
 // Task 8
-console.log('-----> 8 <-----');
-
 const task8 = (nums) => {
     return !!(nums.reduce((acc, num) => {
         if (acc[0].includes(num)) {
@@ -98,5 +76,64 @@ const task8 = (nums) => {
     }, [[], 0]))[1];
 };
 
-console.log(task8([1, 2, 3, 4, 6, 6, 3, 8, 9, 0]));
-console.log(task8([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
+// console.log(task8([1, 2, 3, 4, 6, 6, 3, 8, 9, 0]));
+// console.log(task8([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
+
+// Task 9
+const students = [
+
+    {name: 'Tirion', class: 'B', mark: 3},
+
+    {name: 'Keit', class: 'A', mark: 3},
+
+    {name: 'Ramsey', class: 'A', mark: 4},
+
+    {name: 'Mike', class: 'B', mark: 5},
+
+    {name: 'Alex', class: 'C', mark: 2},
+
+];
+
+const task9A = (students) => {
+    const marks = students.map(student => student.mark);
+    const sum = marks.reduce((acc, mark) => acc + mark);
+    const studentsCount = students.length;
+    const gpa = sum / studentsCount;
+    const max = Math.max(...marks);
+    const min = Math.min(...marks);
+    return {gpa, max, min, sum, studentsCount};
+};
+
+// console.log(task9A(students));
+
+const task9B = (students) => students.reduce((acc, student) => {
+    const cls = student.class;
+    if (!Object.hasOwn(cls)) {
+        acc[cls] = students.filter(student => student.class === cls);
+    }
+    return acc;
+}, {});
+
+// console.log(task9B(students));
+
+// Task 10
+const sites = [
+    'http://google.com',
+    'https://hexlet.io',
+    'yandex.ru',
+    'dzen.ru',
+    'https://ya.ru ',
+    'https://github.com',
+    'http://mail.ru',
+];
+
+const task10 = (sites) => sites.reduce((acc, site) => {
+    if (site.startsWith('https')) {
+        acc.https += 1;
+    } else {
+        acc.http += 1;
+    }
+    return acc;
+}, {'http': 0, 'https': 0});
+
+// console.log(task10(sites));
